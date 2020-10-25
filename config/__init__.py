a464ed48847739d09900156e221d1cd69e2f820b
Ospino <ospino.p@gmail.com>
2020-10-25 19:49:22 +0100

    add timeseries configuration
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -3,3 +3,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PORT = os.getenv("PORT")
+
+INFLUX_HOST = os.getenv("INFLUX_HOST")
+INFLUX_PORT = os.getenv("INFLUX_PORT")
+INFLUX_USER = os.getenv("INFLUX_USER")
+INFLUX_PASS = os.getenv("INFLUX_PASS")
