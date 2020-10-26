0dc11860266cbbbec9397f029386ed3f1bfc9c2d
Ospino <ospino.p@gmail.com>
2020-10-26 21:14:43 +0100

    add argparse setting for verbose
    Signed-off-by: Ospino <ospino.p@gmail.com>

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import tornado.ioloop
 import tornado.web
@@ -17,8 +18,21 @@ def make_app():
     return tornado.web.Application(routes_rules, debug=DEBUG)
 
 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', '-v', action='count', default=1)
+
+    args = parser.parse_args()
+    args.verbose = 70 - (10 * args.verbose) if args.verbose > 0 else 0
+    return args
+
+
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    args = get_arguments()
+    logging.basicConfig(level=args.verbose,
+                        format='%(asctime)s %(levelname)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
     logging.info("Creating app")
     app = make_app()
     logging.info(f"Listening on port {PORT}")
